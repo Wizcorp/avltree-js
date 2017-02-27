bc42729bad6aff47866f81d5ef5e09f989365802
John Ferreira <tatsujinichi@gmail.com>
2017-02-27 15:32:14 +0900

    keep root internal

--- index.js
+++ index.js
@@ -4,30 +4,40 @@ function AvlTree(comparisonFunc) {
 	} else {
 		this._compare = comparisonFunc;
 	}
+	this._root = null;
 }
 module.exports = AvlTree;
 
-AvlTree.prototype.search = function (element, node) {
+// TODO: should this return the node?
+AvlTree.prototype.search = function (element) {
+	return this._search(element, this._root);
+};
+
+AvlTree.prototype._search = function (element, node) {
 	var direction = this._compare(element, node.element);
 	if (direction < 0) {
-		return this.search(element, node.left);
+		return this._search(element, node.left);
 	} else if (direction > 0) {
-		return this.search(element, node.right);
+		return this._search(element, node.right);
 	} else {
 		return node;
 	}
 };
 
-//TODO: internalize and manage root
-AvlTree.prototype.insert = function (element, node) {
+AvlTree.prototype.insert = function (element) {
+	this._root = this._insert(element, this._root);
+};
+
+//TODO: allow duplicates
+AvlTree.prototype._insert = function (element, node) {
 	if (node === null) {
 		return new Node(element);
 	}
 	var direction = this._compare(element, node.element);
 	if (direction < 0) { // go left
-		node.left = this.insert(element, node.left);
+		node.left = this._insert(element, node.left);
 	} else if (direction > 0) { // go right
-		node.right = this.insert(element, node.right);
+		node.right = this._insert(element, node.right);
 	}
 	node.height = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
 
@@ -74,15 +84,20 @@ AvlTree.prototype._leftRotate = function (node) {
 	return r;
 };
 
-// TODO: call from root
-AvlTree.prototype.delete = function (element, node, parent) {
+AvlTree.prototype.delete = function (element) {
+	this._delete(element, this._root, null);
+};
+
+AvlTree.prototype._delete = function (element, node, parent) {
 	var direction = this._compare(element, node.element);
 	if (direction < 0) { // go left
-		return this.delete(element, node.left, node);
+		return this._delete(element, node.left, node);
 	} else if (direction > 0) { // go right
-		return this.delete(element, node.right, node);
+		return this._delete(element, node.right, node);
 	} else { // found our target element
 		if (node.left !== null && node.right !== null) {
+
+
 			var current = node.right; // find the next greatest valued node
 			var parentOfNextGreatest = node;
 			var nextGreatest = null;
@@ -101,6 +116,8 @@ AvlTree.prototype.delete = function (element, node, parent) {
 			} else { // we are in the left child
 				parent.left = nextGreatest;
 			}
+
+
 		} else if (node.left !== null) { // only has left
 			if (parent.right === node) { // we are in the right child
 				parent.right = node.left;
@@ -146,11 +163,7 @@ AvlTree.prototype.getMax = function (element, node) {
 AvlTree.prototype._balance = function (node) {
 	var balance = getHeight(node.left) - getHeight(node.right);
 	if (balance < 0) {
-		// TODO: rr
-		// TODO: ll
-
-		// TODO: rl
-		// TODO: lr
+		//TODO: maybe trinode
 
 		// TODO: update height
 	} else if (balance > 0) {
@@ -158,6 +171,62 @@ AvlTree.prototype._balance = function (node) {
 	}
 	return node;
 };
+
+//TODO: refactor out parent
+AvlTree.prototype.triNodeRestructure = function (x, y, z) {
+	var a, b, c;
+	if (z.element <= x.element && x.element <= y.element) {
+		a = z;
+		b = x;
+		c = y;
+	}
+	if (z.element >= x.element && x.element >= y.element) {
+		a = y;
+		b = x;
+		c = z;
+	}
+	if (z.element <= y.element && y.element <= x.element) {
+		a = z;
+		b = y;
+		c = x;
+	}
+	if (z.element >= y.element && y.element >= x.element) {
+		a = x;
+		b = y;
+		c = z;
+	}
+
+	if (z === this._root) {
+		this._root = b;
+		b.parent = null
+	} else {
+		if (z.parent.left === z) {
+			makeLeftChild(z.parent, b);
+		} else {
+			makeRightChild(z.parent, b);
+		}
+	}
+
+	if (b.left !== x && b.left !== y && b.left !== z) {
+		makeRightChild(a, b.left);
+	}
+	if (b.right !== x && b.right !== y && b.right !== z) {
+		makeLeftChild(c, b.right);
+	}
+	makeLeftChild(b, a);
+	makeRightChild(b, c);
+};
+
+function makeLeftChild(a, b) {
+	a.leftChild = b;
+	b.parent = a;
+}
+
+function makeRightChild(a, b) {
+	a.rightchild = b;
+	b.parent = a;
+}
+//TODO: refactor out parent
 
 AvlTree.prototype.forEach = function (node, func) {
 	this.forEach(node.left, func);
--- test/test.js
+++ test/test.js
@@ -8,64 +8,64 @@ describe('avltree-js tests', function() {
 			tree = new AvlTree();
 		});
 		it('should contain the inserted value', function () {
-			var root = tree.insert(1, null);
-			assert.strictEqual(root.element, 1);
-			tree.insert(2, root);
-			assert.strictEqual(root.right.element, 2);
-			assert.strictEqual(root.height, 2);
+			tree.insert(1);
+			assert.strictEqual(tree._root.element, 1);
+			tree.insert(2);
+			assert.strictEqual(tree._root.right.element, 2);
+			assert.strictEqual(tree._root.height, 2);
 		});
 		it('should balance a right right heavy tree', function () {
-			var root = tree.insert(1, null);
-			root = tree.insert(2, root);
-			root = tree.insert(3, root);
-			assert.strictEqual(root.element, 2);
-			assert.strictEqual(root.left.element, 1);
-			assert.strictEqual(root.right.element, 3);
-			assert.strictEqual(root.height, 2);
-			assert.strictEqual(root.left.height, 1);
-			assert.strictEqual(root.right.height, 1);
+			tree.insert(1);
+			tree.insert(2);
+			tree.insert(3);
+			assert.strictEqual(tree._root.element, 2);
+			assert.strictEqual(tree._root.left.element, 1);
+			assert.strictEqual(tree._root.right.element, 3);
+			assert.strictEqual(tree._root.height, 2);
+			assert.strictEqual(tree._root.left.height, 1);
+			assert.strictEqual(tree._root.right.height, 1);
 		});
 		it('should balance a left left heavy tree', function () {
-			var root = tree.insert(3, null);
-			root = tree.insert(2, root);
-			root = tree.insert(1, root);
-			assert.strictEqual(root.element, 2);
-			assert.strictEqual(root.left.element, 1);
-			assert.strictEqual(root.right.element, 3);
-			assert.strictEqual(root.height, 2);
-			assert.strictEqual(root.left.height, 1);
-			assert.strictEqual(root.right.height, 1);
+			tree.insert(3);
+			tree.insert(2);
+			tree.insert(1);
+			assert.strictEqual(tree._root.element, 2);
+			assert.strictEqual(tree._root.left.element, 1);
+			assert.strictEqual(tree._root.right.element, 3);
+			assert.strictEqual(tree._root.height, 2);
+			assert.strictEqual(tree._root.left.height, 1);
+			assert.strictEqual(tree._root.right.height, 1);
 		});
 		it('should balance a right left heavy tree', function () {
-			var root = tree.insert(3, null);
-			root = tree.insert(1, root);
-			root = tree.insert(2, root);
-			assert.strictEqual(root.element, 2);
-			assert.strictEqual(root.left.element, 1);
-			assert.strictEqual(root.right.element, 3);
-			assert.strictEqual(root.height, 2);
-			assert.strictEqual(root.left.height, 1);
-			assert.strictEqual(root.right.height, 1);
+			tree.insert(3);
+			tree.insert(1);
+			tree.insert(2);
+			assert.strictEqual(tree._root.element, 2);
+			assert.strictEqual(tree._root.left.element, 1);
+			assert.strictEqual(tree._root.right.element, 3);
+			assert.strictEqual(tree._root.height, 2);
+			assert.strictEqual(tree._root.left.height, 1);
+			assert.strictEqual(tree._root.right.height, 1);
 		});
 		it('should backtrack and balance', function () {
-			var root = tree.insert(1, null);
-			root = tree.insert(2, root);
-			root = tree.insert(3, root);
-			root = tree.insert(4, root);
-			root = tree.insert(5, root);
-			root = tree.insert(6, root);
-			assert.strictEqual(root.element, 4);
-			assert.strictEqual(root.height, 3);
-			assert.strictEqual(root.left.element, 2);
-			assert.strictEqual(root.left.height, 2);
-			assert.strictEqual(root.left.left.element, 1);
-			assert.strictEqual(root.left.left.height, 1);
-			assert.strictEqual(root.left.right.element, 3);
-			assert.strictEqual(root.left.right.height, 1);
-			assert.strictEqual(root.right.element, 5);
-			assert.strictEqual(root.right.height, 2);
-			assert.strictEqual(root.right.right.element, 6);
-			assert.strictEqual(root.right.right.height, 1);
+			tree.insert(1);
+			tree.insert(2);
+			tree.insert(3);
+			tree.insert(4);
+			tree.insert(5);
+			tree.insert(6);
+			assert.strictEqual(tree._root.element, 4);
+			assert.strictEqual(tree._root.height, 3);
+			assert.strictEqual(tree._root.left.element, 2);
+			assert.strictEqual(tree._root.left.height, 2);
+			assert.strictEqual(tree._root.left.left.element, 1);
+			assert.strictEqual(tree._root.left.left.height, 1);
+			assert.strictEqual(tree._root.left.right.element, 3);
+			assert.strictEqual(tree._root.left.right.height, 1);
+			assert.strictEqual(tree._root.right.element, 5);
+			assert.strictEqual(tree._root.right.height, 2);
+			assert.strictEqual(tree._root.right.right.element, 6);
+			assert.strictEqual(tree._root.right.right.height, 1);
 		});
 	});
 	describe('search', function () {
@@ -75,24 +75,24 @@ describe('avltree-js tests', function() {
 		});
 		it('should find the desired element', function () {
 			var root = tree.insert(1, null);
-			root = tree.insert(3, root);
-			root = tree.insert(4, root);
-			root = tree.insert(8, root);
-			root = tree.insert(2, root);
-			root = tree.insert(9, root);
-			root = tree.insert(5, root);
-			root = tree.insert(11, root);
-			root = tree.insert(7, root);
-			root = tree.insert(6, root);
-			root = tree.insert(10, root);
+			tree.insert(3);
+			tree.insert(4);
+			tree.insert(8);
+			tree.insert(2);
+			tree.insert(9);
+			tree.insert(5);
+			tree.insert(11);
+			tree.insert(7);
+			tree.insert(6);
+			tree.insert(10);
 			var target = 6;
-			var targetNode = tree.search(target, root);
+			var targetNode = tree.search(target);
 			assert.strictEqual(targetNode.element, target);
-			root = tree.insert(12, root);
-			root = tree.insert(13, root);
-			root = tree.insert(15, root);
-			root = tree.insert(16, root);
-			root = tree.insert(14, root);
+			tree.insert(12);
+			tree.insert(13);
+			tree.insert(15);
+			tree.insert(16);
+			tree.insert(14);
 			var targetNode = tree.search(target, root);
 			assert.strictEqual(targetNode.element, target);
 		});
@@ -103,15 +103,24 @@ describe('avltree-js tests', function() {
 			tree = new AvlTree();
 		});
 		it('should remove a leaf element from the tree', function () {
-			var root = tree.insert(1, null);
-			root = tree.insert(2, root);
-			tree.delete(2, root, null);
-			assert.strictEqual(root.right, null);
-			assert.strictEqual(root.element, 1);
-			assert.strictEqual(root.height, 1);
+			tree.insert(1);
+			tree.insert(2);
+			tree.delete(2);
+			assert.strictEqual(tree._root.right, null);
+			assert.strictEqual(tree._root.element, 1);
+			assert.strictEqual(tree._root.height, 1);
 		});
 		it('should delete the root from the tree', function () {
-			
+
+		});
+		it('should remove an element with only left children', function () {
+
+		});
+		it('should remove an element with only right children', function () {
+
+		});
+		it('should remove an element with two children', function () {
+
 		});
 	});
 });
